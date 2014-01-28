763cdd811a855f1627e2125f86516c3a50fad45c
Matthew O. Smith <matt@m0smith.com>
2014-01-28 16:57:42 -0700

    Added the ability to search maven central for an artifact

--- maven-pom-mode.el
+++ maven-pom-mode.el
@@ -37,6 +37,8 @@
 ;;; Code:
 
 
+(autoload 'json-read-from-string "json")
+
 (defgroup maven-pom-mode nil
   "Major mode for editting pom.xml files")
 
@@ -53,3 +55,97 @@
 		  (expand-file-name (format "%s/%s" 
 					    maven-pom-mode-install-directory
 					    "schemas.xml")))))
+
+;;;; mvn-search.el --- do searches against search.maven.org
+;;;; From https://github.com/upgradingdave/maven-mode/blob/master/mvn-search.el
+
+
+(defvar maven-pom-search-maven-org-url "http://search.maven.org")
+(defvar maven-pom-results-buffer "*maven-pom-results*")
+
+;;;; Getters
+;; The Search functions below convert json to vectors. Use these
+;; getters to extract info from the vectors
+
+(defun maven-pom-coords-from-alist (alist)
+  "Given a alist representing a maven artifact, return coords"
+  (cdr (assoc 'id alist)))
+
+(defun maven-pom-artifactId-from-alist (alist)
+  "Given a alist representing a maven artifact, return artifactId"
+  (cdr (assoc 'a alist)))
+
+(defun maven-pom-groupId-from-alist (alist)
+  "Given a alist representing a maven artifact, return groupId"
+  (cdr (assoc 'g alist)))
+
+(defun maven-pom-latest-version-from-alist (alist)
+  "Given a alist representing a maven artifact, return version"
+  (cdr (assoc 'latestVersion alist)))
+
+(defun maven-pom-version-from-alist (alist)
+  "Given a alist representing a maven artifact, return version"
+  (cdr (assoc 'v alist)))
+
+;;;; Search functions
+
+(defun maven-pom-search-get-json (url)
+  "Make a request, get response, strip response headers, pass back json"
+  (let* ((buffer 
+          (url-retrieve-synchronously url))
+         (json (concat "{" (with-current-buffer buffer
+                             (goto-char (point-min))
+                             (substring (buffer-string) (- (search-forward "{") 1))))))
+    (kill-buffer buffer)
+    json))
+
+(defun maven-pom-search-by-term (search-term)
+  "Do search against search.maven.org for SEARCH-TERM"
+  (maven-pom-search-get-json 
+   (url-generic-parse-url 
+    (concat maven-pom-search-maven-org-url "/solrsearch/select?q=\"" 
+            search-term "\"&rows=20&wt=json"))))
+
+(defun maven-pom-search-for-versions (groupId artifactId)
+  "Given GROUPID and ARTIFACTID, return list of all available versions"
+  (maven-pom-search-get-json 
+   (url-generic-parse-url
+    (concat maven-pom-search-maven-org-url "/solrsearch/select?q=g:\"" groupId 
+            "\"+AND+a:\"" artifactId "\"&core=gav&rows=20&wt=json"))))
+
+(defun maven-pom-search-json-to-vector (json)
+  "Convert json into a vector. Use getters to extract info from vector"
+  (let ((json-object-type 'alist))
+    (cdr (assoc 'docs (assoc 'response (json-read-from-string json))))))
+
+(defun maven-pom-search-completing-groupIds (search-term)
+  "Search for artifact by search term"
+  (interactive "MSearch: ")
+  (let ((groupIds 
+         (mapcar (lambda (x) 
+                   (concat 
+                    (maven-pom-groupId-from-alist x) ":"
+                    (maven-pom-artifactId-from-alist x)))
+                 (maven-pom-search-json-to-vector (maven-pom-search-by-term search-term)))))
+    (completing-read "Choose artifact:" groupIds nil t)))
+
+(defun maven-pom-search-completing-versions (coord)
+  "Search for all versions of coord <groupId:artifactId>"
+  (interactive "MSearch: ")
+  (let* ((artifactId (cadr (split-string coord ":")))
+         (groupId (car (split-string coord ":")))
+         (versions 
+          (mapcar (lambda (x) 
+                    (concat 
+                     (maven-pom-groupId-from-alist x) ":"
+                     (maven-pom-artifactId-from-alist x) ":"
+                     (maven-pom-version-from-alist x) ))
+                  (maven-pom-search-json-to-vector (maven-pom-search-for-versions groupId artifactId)))))
+    (completing-read "Choose Version: " versions nil t coord)))
+
+(defun maven-pom-search (search-term)
+  "Search for artifact by search term and return the GAV"
+  (interactive "MSearch: ")
+  (maven-pom-search-completing-versions (maven-pom-search-completing-groupIds search-term)))
+
+
